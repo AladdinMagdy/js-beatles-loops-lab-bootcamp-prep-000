357c14f591232c0c436d298560a00e2d8536b9d0
Aladdin Magdy <aladdinmagdysaber167@commerce.helwan.edu.eg>
2018-02-11 15:42:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+  let arr = [];
+  for(let i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr;
+}
+function johnLennonFacts(array){
+  let i = 0;
+  while(i < array.length){
+    array[i] += '!!!';
+    i++;
+  }
+  return array;
+}
+function iLoveTheBeatles(num){
+  let arr = [];
+  let i = 0;
+  do{
+    arr.push("I love the Beatles!")
+  } while(i < num && num < 15){
+    i++
+  }
+  return arr;
+}
